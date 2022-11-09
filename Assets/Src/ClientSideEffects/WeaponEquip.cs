1a34daba94a1c9f7dd0ec28fd837e18b022c9c70
david-simoes-93 <david.simoes@ua.pt>
2022-11-09 19:42:26 +0000

    Weapons now attach to local camera

--- Assets/Src/ClientSideEffects/WeaponEquip.cs
+++ Assets/Src/ClientSideEffects/WeaponEquip.cs
@@ -15,20 +15,42 @@ internal class WeaponEquip : ILocalEffect
     /// <param name="weapon">Weapon cast code</param>
     internal WeaponEquip(UnitEntity parent, Globals.CastCode weapon)
     {
+        Transform parent_transform;
+        if (parent.Uid == ClientGameLoop.CGL.UnitEntity.Uid)
+        {
+            parent_transform = parent.CameraTransform();
+        }
+        else
+        {
+            parent_transform = parent.UnitTransform();
+        }
+
         switch (weapon)
         {
             case Globals.CastCode.SniperChooseWeaponOne:
-                myGameObject_ = Object.Instantiate(Globals.kSniperWeaponOne, parent.UnitTransform());
+                myGameObject_ = Object.Instantiate(Globals.kSniperWeaponOne, parent_transform);
+                if (parent.Uid == ClientGameLoop.CGL.UnitEntity.Uid)
+                {
+                    myGameObject_.transform.localPosition = new Vector3(0.1f, -0.4f, 0.1f);
+                }
                 break;
             case Globals.CastCode.SniperChooseWeaponTwo:
-                myGameObject_ = Object.Instantiate(Globals.kSniperWeaponTwo, parent.UnitTransform());
+                myGameObject_ = Object.Instantiate(Globals.kSniperWeaponTwo, parent_transform);
+                if (parent.Uid == ClientGameLoop.CGL.UnitEntity.Uid)
+                {
+                    myGameObject_.transform.localPosition = new Vector3(0.2f, -0.4f, 0.4f);
+                }
                 break;
             case Globals.CastCode.SniperChooseWeaponThree:
-                myGameObject_ = Object.Instantiate(Globals.kSniperWeaponThree, parent.UnitTransform());
+                myGameObject_ = Object.Instantiate(Globals.kSniperWeaponThree, parent_transform);
+                if (parent.Uid == ClientGameLoop.CGL.UnitEntity.Uid)
+                {
+                    myGameObject_.transform.localPosition = new Vector3(0.1f, -0.3f, 0.7f);
+                }
                 break;
             default:
                 GameDebug.Log("Unknown weapon selected!");
-                myGameObject_ = Object.Instantiate(Globals.kEmptyPrefab, parent.UnitTransform());
+                myGameObject_ = Object.Instantiate(Globals.kEmptyPrefab, parent_transform);
                 break;
         }
         //myGameObject_.transform.position = src;
